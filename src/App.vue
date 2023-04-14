30528fa88a2ed4ae430f3890ea3fb22fc1e6dac2
bourbakiii <gabisov03912@gmail.com>
2023-04-14 23:35:55 +0300

    Scss installed & base layout deleted

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+
+  Some text right there
+  <br>
+  {{some_number}}
+  <br>
+  <button @click="some_number++">Add one</button>
+
+</template>
+
+<script setup lang="ts">
+import {ref} from "vue";
+let some_number = ref<number>(0);
+
+console.log(typeof some_number)
+</script>=
